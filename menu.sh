bd9001c7c86faef6dde395deac163d9a7665bd66
Br!z@l0k@ <57237342+ianmsfvenom@users.noreply.github.com>
2021-03-12 07:41:59 -0300

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -1,13 +1,28 @@
 #!bin/bash
-
-apt install ruby -y
-gem install lolcat
 apt install figlet 
 CYAN="\033[1;36m"
 PURPLE="\033[1;35m"
 GREEN="\033[1;32m"
 RED='\033[1;31m'
 NC='\033[0m'
+
+if [-e "/data/data/com.termux/files/usr/bin/ruby"]; then
+	echo $GREEN "ruby detectado prosseguindo..."
+	sleep 1
+	if [-e "/data/data/com.termux/files/usr/bin/lolcat"]; then
+		echo $GREEN "lolcat detectado prosseguindo..."
+		sleep 1
+	else
+		echo $RED "lolcat não detectado, instalando..."
+		gem install lolcat
+	fi
+else
+	echo $RED "Você não instalou o ruby, instalando..."
+	sleep 1
+	apt install ruby
+	gem install lolcat
+fi
+
 clear
 
 figlet -c -f slant -t 'Bot Whatsapp Termux' | lolcat 
